9cd7677357d53c26c98d2e17a2df7ceec4863280
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-06 01:30:08 +0900

    feat 기본 액티비티 및 viewmodel 초기화

--- presentation/src/main/java/com/sangrok/afreecatv/MainActivity.kt
+++ presentation/src/main/java/com/sangrok/afreecatv/MainActivity.kt
@@ -3,32 +3,21 @@ package com.sangrok.afreecatv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.sangrok.afreecatv.ui.theme.AfreecatvTheme
+import androidx.activity.viewModels
+import com.sangrok.afreecatv.screen.MainScreen
+import com.sangrok.afreecatv.theme.AfreecaTvTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AfreecatvTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            AfreecaTvTheme {
+                MainScreen(
+                    viewModel = viewModel
+                )
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
---
+++ presentation/src/main/java/com/sangrok/afreecatv/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.sangrok.afreecatv
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel()
