6d7fd89689f4a82133b831686126393eb69e4e04
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-05 18:11:45 +0900

    feat: 사용할 theme 정의

--- presentation/src/main/java/com/sangrok/afreecatv/ui/theme/Type.kt
+++ presentation/src/main/java/com/sangrok/afreecatv/ui/theme/Type.kt
@@ -1,38 +1,13 @@
 package com.sangrok.afreecatv.ui.theme
 
-import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import com.sangrok.presentation.R
 
 val Pretendard = FontFamily(
     Font(R.font.pretendard_bold, FontWeight.Bold),
     Font(R.font.pretendard_semibold, FontWeight.SemiBold),
     Font(R.font.pretendard_medium, FontWeight.Medium),
-    Font(R.font.pretendard_regular)
-)
-
-
-// Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    Font(R.font.pretendard_regular, FontWeight.Normal)
 )
