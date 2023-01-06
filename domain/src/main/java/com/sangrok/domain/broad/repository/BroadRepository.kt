3eb8b4f1f0e14b0174338de03c8ec34379bc2ba4
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-07 03:29:29 +0900

    feat: PagingData WIP

--- domain/src/main/java/com/sangrok/domain/broad/repository/BroadRepository.kt
+++ domain/src/main/java/com/sangrok/domain/broad/repository/BroadRepository.kt
@@ -1,14 +1,16 @@
 package com.sangrok.domain.broad.repository
 
+import androidx.paging.PagingData
 import com.sangrok.domain.broad.model.Broad
 import com.sangrok.domain.broad.model.Category
+import kotlinx.coroutines.flow.Flow
 
 interface BroadRepository {
     suspend fun getBroads(
         clientId: String,
         categoryId: String,
         page: Int
-    ): List<Broad>
+    ): Flow<PagingData<Broad>>
 
     suspend fun getCategories(
         clientId: String,
