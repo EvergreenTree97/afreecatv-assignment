8e80fab951a2ca82b6db88e3508b098f02630f8e
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-07 01:17:13 +0900

    feat: typo 및 color 작성

--- presentation/src/main/java/com/sangrok/afreecatv/theme/Color.kt
+++ presentation/src/main/java/com/sangrok/afreecatv/theme/Color.kt
@@ -3,3 +3,7 @@ package com.sangrok.afreecatv.theme
 import androidx.compose.ui.graphics.Color
 
 val MainBlue = Color(0xFF0545B0)
+val HalfBlue = Color(0xFF0A4E8D)
+
+val Black = Color(0xFF222222)
+val Red = Color(0xFFFF2929)
--- presentation/src/main/java/com/sangrok/afreecatv/theme/Type.kt
+++ presentation/src/main/java/com/sangrok/afreecatv/theme/Type.kt
@@ -1,8 +1,10 @@
 package com.sangrok.afreecatv.theme
 
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.sangrok.presentation.R
 
 val Pretendard = FontFamily(
@@ -10,4 +12,42 @@ val Pretendard = FontFamily(
     Font(R.font.pretendard_semibold, FontWeight.SemiBold),
     Font(R.font.pretendard_medium, FontWeight.Medium),
     Font(R.font.pretendard_regular, FontWeight.Normal)
+)
+
+val HeadLineSemiBold = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 16.sp,
+)
+
+val HeadLineRegular = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+)
+
+val BodyMedium = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp,
+)
+
+
+val CaptionRegular = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Normal,
+    fontSize = 10.sp,
+)
+
+
+val Caption2Regular = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Normal,
+    fontSize = 8.sp,
+)
+
+val Caption3Regular = TextStyle(
+    fontFamily = Pretendard,
+    fontWeight = FontWeight.Normal,
+    fontSize = 6.sp,
 )
