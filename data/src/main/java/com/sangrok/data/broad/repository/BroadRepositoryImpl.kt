de5e3979536246a7326061ba201cab3b3fc0e865
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-08 22:40:18 +0900

    feat: enableHolder false

--- data/src/main/java/com/sangrok/data/broad/repository/BroadRepositoryImpl.kt
+++ data/src/main/java/com/sangrok/data/broad/repository/BroadRepositoryImpl.kt
@@ -24,6 +24,7 @@ class BroadRepositoryImpl @Inject constructor(
             config = PagingConfig(
                 pageSize = PageSize,
                 initialLoadSize = InitialLoadSize,
+                enablePlaceholders = false
             ),
             pagingSourceFactory = {
                 BroadPagingSource(
