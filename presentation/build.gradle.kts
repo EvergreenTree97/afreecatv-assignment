c0d037d51c948dafc04d15a3b712625856b84081
EvergreenTree97 <tkdfhr7497@naver.com>
2023-01-08 21:17:25 +0900

    chore: client id 관리, menifest 정의

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
@@ -11,10 +13,10 @@ android {
 
     buildTypes {
         debug {
-
+            buildConfigField("String", "CLIENT_ID", getLocalProperty("CLIENT_ID"))
         }
         release {
-
+            buildConfigField("String", "CLIENT_ID", getLocalProperty("CLIENT_ID"))
         }
     }
 
@@ -38,8 +40,14 @@ android {
 dependencies {
     kapt(Dependencies.Hilt.Kapt)
     implementation(project(Modules.Domain))
+    implementation(project(Modules.Core))
     implementation(Dependencies.Hilt.Android)
+    implementation(Dependencies.Kotlin.ImmutableCollections)
     Dependencies.Essential.forEach(::implementation)
     Dependencies.Compose.forEach(::implementation)
     Dependencies.Ktx.forEach(::implementation)
+}
+
+fun getLocalProperty(propertyKey: String): String {
+    return gradleLocalProperties(rootDir).getProperty(propertyKey)
 }
